78d0bdaad11625f4ab01bc213cc20af169cc12a0
aryanishan <myteam034221@gmail.com>
2026-02-22 14:52:13 +0530

    Play using keyboard feature added

--- javascript/script.js
+++ javascript/script.js
@@ -24,6 +24,7 @@ function autoPlay() {
     }
 }
 
+
 let result = '';
 function pickComputerMove() {
     const randomNumber = Math.random();
@@ -39,6 +40,7 @@ function pickComputerMove() {
     return computerMove;
 }
 
+// let youPick = '';
 function resultShown(youPick) {
     const computerMove = pickComputerMove();
 
@@ -82,6 +84,37 @@ function resultShown(youPick) {
     updateScore();
     document.querySelector('.js-result').innerHTML = result;
     document.querySelector('.js-moves').innerHTML = `You <img src="images/${youPick}-emoji.png" alt="rock-image" class="move-icon"><img
-            src="images/${computerMove}-emoji.png" alt="scissors-image" class="move-icon"> Computer`;
+    src="images/${computerMove}-emoji.png" alt="scissors-image" class="move-icon"> Computer`;
 
 }
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+    resultShown('rock');
+});
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+    resultShown('paper');
+});
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+    resultShown('scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        resultShown('rock');
+    } else if (event.key === 'p') {
+        resultShown('paper');
+    } else if (event.key === 's') {
+        resultShown('scissors');
+    }
+});
+
+document.querySelector('.auto-play-button').addEventListener('click', () => {
+    autoPlay();
+});
+
+document.querySelector('.reset-score-button').addEventListener('click', () => {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScore();
+});
